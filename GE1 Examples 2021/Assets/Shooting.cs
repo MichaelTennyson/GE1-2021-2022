cac6d0f505325830c1073b0793fa29560e4738d1
c18302166 <c18302166@mytudublin.ie>
2021-10-15 17:11:50 +0100

    Finished lab4

---
+++ GE1 Examples 2021/Assets/Shooting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float fireRate = 5;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnEnable()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    bool shooting = false;
+
+    System.Collections.IEnumerator Shoot()
+    {
+        while(true)
+        {
+            if (Input.GetAxis("Fire1") > 0 )     
+            {
+                GameObject b = GameObject.Instantiate<GameObject>(bulletPrefab);
+                b.transform.position = bulletSpawn.transform.position;
+                b.transform.rotation = bulletSpawn.rotation;
+                yield return new WaitForSeconds(1 / (float) fireRate);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
